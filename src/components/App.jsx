350438c108febd6542df6af880e43828e0651d8b
Oderen~ <oleksandr.deren02@gmail.com>
2023-09-26 20:22:10 +0300

    changed text color

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,13 @@ export const App = () => {
   return (
     <div
       style={{
+        color: 'tomato',
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
