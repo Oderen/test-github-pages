0872dceb2459ace6b4318a47a01b145d2ec0f8b1
Oderen~ <oleksandr.deren02@gmail.com>
2023-09-26 21:11:41 +0300

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import { Routes, Route, Link } from 'react-router-dom';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        color: 'tomato',
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-      }}
-    >
-      React homework template
+    <div>
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/second">Second</Link>
+        <Link to="/third">Third</Link>
+        <div>Changes was made</div>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<MainPage />} />
+            <Route path="second" element={<SecondPage />} />
+            <Route path="third" element={<ThirdPage />} />
+          </Route>
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
     </div>
   );
 };
