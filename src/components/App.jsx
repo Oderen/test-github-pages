ded32a079adc894a5846811f3d2d375700af0466
Oderen~ <oleksandr.deren02@gmail.com>
2023-09-26 21:15:26 +0300

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,10 @@
 import { Routes, Route, Link } from 'react-router-dom';
 
+import MainPage from './MainPage';
+import SecondPage from './SecondPage';
+import ThirdPage from './ThirdPage';
+import NotFound from './NotFound';
+
 export const App = () => {
   return (
     <div>
@@ -11,11 +16,9 @@ export const App = () => {
       </header>
       <main>
         <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<MainPage />} />
-            <Route path="second" element={<SecondPage />} />
-            <Route path="third" element={<ThirdPage />} />
-          </Route>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/second" element={<SecondPage />} />
+          <Route path="/third" element={<ThirdPage />} />
 
           <Route path="*" element={<NotFound />} />
         </Routes>
